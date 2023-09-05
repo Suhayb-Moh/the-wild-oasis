262e545061eb277b6a7d16cac75876d9f3f20841
Suhayb Mohamed <suhaybmaxamed@gmail.com>
2023-09-05 17:07:56 +0300

    added dark mode

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+import { supabaseUrl } from "../services/supabase";
 
 export async function signUp({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -51,4 +52,39 @@ export async function logout() {
   if (error) {
     throw new Error(error.message);
   }
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. update password OR fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!avatar) return data;
+  // 2. upload the avatar image
+  const fileName = `avatar=${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+  // 3. update the avatar in the user
+  const { data: updatedUser, error: errorUpdatedUser } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (errorUpdatedUser) {
+    throw new Error(errorUpdatedUser.message);
+  }
+  return updatedUser;
 }
